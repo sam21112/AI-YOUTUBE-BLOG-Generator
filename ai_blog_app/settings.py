db9e5d493f75332a04992f4b7bce15fb19d8ea22
samarth raipal <samarthraipal@gmail.com>
2024-11-21 17:57:12 -0500

    Add Render domain to ALLOWED_HOSTS

--- ai_blog_app/settings.py
+++ ai_blog_app/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ai-youtube-blog-generator-2.onrender.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
